47a3a8e654d01b165fe5dd3253cab6579f2f20c8
npdk04 <khoa1903204@gmail.com>
2025-06-11 13:18:30 +0700

    avc

--- src/app/user-api.service.ts
+++ src/app/user-api.service.ts
@@ -79,11 +79,5 @@ export class UserAPIService {
       headers: this.getHeaders()
     }).pipe(catchError(this.handleError));
   }
-  forgotPassword(email: string): Observable<any> {
-    return this.http.post<any>(
-      `${this.apiUrl}/users/forgot-password`,
-      { email },
-      { headers: this.getHeaders() }
-    ).pipe(catchError(this.handleError));
-  }
+  
 }
